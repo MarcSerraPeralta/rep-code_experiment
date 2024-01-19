4508d9f455eeb09a28a7c3507917a75e5c39eab5
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-01-19 17:31:45 +0100

    Add noise models module

---
+++ rep_code/models/__init__.py
@@ -0,0 +1,3 @@
+from .library import DecoherenceNoiseModelExp, NoiselessModelExp, CircuitNoiseModelExp
+
+__all__ = ["DecoherenceNoiseModelExp", "NoiselessModelExp", "CircuitNoiseModelExp"]
---
+++ rep_code/models/library.py
@@ -0,0 +1,36 @@
+from typing import Sequence, Iterator
+
+from stim import CircuitInstruction
+
+from surface_sim.models import DecoherenceNoiseModel, NoiselessModel, CircuitNoiseModel
+
+
+class DecoherenceNoiseModelExp(DecoherenceNoiseModel):
+    def x_echo(self, qubits: Sequence[str]) -> Iterator[CircuitInstruction]:
+        name = "X_ECHO"
+        if self._sym_noise:
+            duration = 0.5 * self.gate_duration(name)
+
+            yield from self.idle(qubits, duration)
+            yield CircuitInstruction("X", targets=self.get_inds(qubits))
+            yield from self.idle(qubits, duration)
+        else:
+            duration = self.gate_duration(name)
+
+            yield CircuitInstruction(name, targets=self.get_inds(qubits))
+            yield from self.idle(qubits, duration)
+
+
+class NoiselessModelExp(NoiselessModel):
+    def x_echo(self, qubits: Sequence[str]) -> Iterator[CircuitInstruction]:
+        yield CircuitInstruction("X", self.get_inds(qubits))
+
+
+class CircuitNoiseModelExp(CircuitNoiseModel):
+    def x_echo(self, qubits: Sequence[str]) -> Iterator[CircuitInstruction]:
+        inds = self.get_inds(qubits)
+        yield CircuitInstruction("X", inds)
+
+        for qubit, ind in zip(qubits, inds):
+            prob = self.param("sq_error_prob", qubit)
+            yield CircuitInstruction("DEPOLARIZE1", [ind], [prob])
