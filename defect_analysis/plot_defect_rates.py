de78005a785e7b6297c74c76d0cf2694457256ba
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-02-14 12:35:05 +0100

    Add defect rates from state combination

--- defect_analysis/plot_defect_rates.py
+++ defect_analysis/plot_defect_rates.py
@@ -1,6 +1,7 @@
 print("Importing libraries...")
 import pathlib
 import yaml
+from copy import deepcopy
 
 import xarray as xr
 import matplotlib.pyplot as plt
@@ -15,7 +16,7 @@ OUTPUT_DIR = pathlib.Path(
     "/scratch/marcserraperal/projects/20231220-repetition_code_dicarlo_lab/defect_analysis"
 )
 
-EXP_NAME = "20230119_initial_data_d3_s010_combined"
+EXP_NAME = "20230119_initial_data_d3"
 
 DEFECTS_NAME = "defects_DecayLinearClassifierFit"
 
@@ -27,46 +28,79 @@ with open(DATA_DIR / EXP_NAME / "config_data.yaml", "r") as file:
     config_data = yaml.safe_load(file)
 
 STRING_DATA = config_data["string_data_options"]
+STATES = STRING_DATA.pop("state")
 
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+# remove state from directory names
+new_config_data = deepcopy(config_data)
+new_config_data["string_data_options"] = STRING_DATA
+new_config_data["data"] = new_config_data["data"].replace("_s{state}", "")
+new_config_data["config"] = new_config_data["config"].replace("_s{state}", "")
+new_config_data["readout_calibration"] = new_config_data["readout_calibration"].replace(
+    "_s{state}", ""
+)
 
-colors = [
-    "#1f77b4",
-    "#ff7f0e",
-    "#2ca02c",
-    "#d62728",
-    "#9467bd",
-    "#8c564b",
-    "#e377c2",
-    "#7f7f7f",
-]
+(OUTPUT_DIR / EXP_NAME).mkdir(parents=True, exist_ok=True)
+with open(OUTPUT_DIR / EXP_NAME / "config_data.yaml", "w") as file:
+    yaml.dump(new_config_data, file, default_flow_style=False)
+
+colors = {
+    "Z1": "#1f77b4",
+    "Z2": "#ff7f0e",
+    "Z3": "#2ca02c",
+    "Z4": "#d62728",
+    "X1": "#9467bd",
+    "X2": "#8c564b",
+    "X3": "#e377c2",
+    "X4": "#7f7f7f",
+}
 
 print("\n", end="")  # for printing purposes
 
 for element in sequence_generator(STRING_DATA):
-    data_dir = DATA_DIR / EXP_NAME / config_data["data"].format(**element)
-    output_dir = OUTPUT_DIR / EXP_NAME / config_data["data"].format(**element)
+    output_dir = OUTPUT_DIR / EXP_NAME / new_config_data["data"].format(**element)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    defects_xr = xr.load_dataset(data_dir / f"{DEFECTS_NAME}.nc")
-    defects = defects_xr.defects
-    final_defects = defects_xr.final_defects
+    defects_combined = []
+    final_defects_combined = []
 
-    kargs_plot = {
-        q: dict(color=colors[k]) for k, q in enumerate(defects_xr.anc_qubit.values)
-    }
+    for state in STATES:
+        data_dir = (
+            DATA_DIR / EXP_NAME / config_data["data"].format(**element, state=state)
+        )
+
+        defects_xr = xr.load_dataset(data_dir / f"{DEFECTS_NAME}.nc")
+        defects = defects_xr.defects
+        final_defects = defects_xr.final_defects
+
+        kargs_plot = {q: dict(color=colors[q]) for q in defects_xr.anc_qubit.values}
+
+        fig, ax = plt.subplots()
+
+        plot_average_defect_rate(ax, defects, final_defects)
+        plot_defect_rates(ax, defects, final_defects, **kargs_plot)
+
+        fig.tight_layout()
+        fig.savefig(output_dir / f"{DEFECTS_NAME}_rates_s{state}.pdf", format="pdf")
+
+        plt.close()
+
+        defects_combined.append(defects)
+        final_defects_combined.append(final_defects)
+
+        print(f"\033[F\033[K{data_dir}", flush=True)
+
+    # plot combined defect rates
+    defects_combined = xr.concat(defects_combined, dim="shot")
+    final_defects_combined = xr.concat(final_defects_combined, dim="shot")
+
+    kargs_plot = {q: dict(color=colors[q]) for q in defects_xr.anc_qubit.values}
 
     fig, ax = plt.subplots()
 
-    plot_average_defect_rate(ax, defects, final_defects)
-    plot_defect_rates(ax, defects, final_defects, **kargs_plot)
+    plot_average_defect_rate(ax, defects_combined, final_defects_combined)
+    plot_defect_rates(ax, defects_combined, final_defects_combined, **kargs_plot)
 
     fig.tight_layout()
-    fig.savefig(output_dir / f"{DEFECTS_NAME}_rates.pdf", format="pdf")
+    fig.savefig(output_dir / f"{DEFECTS_NAME}_rates_combined.pdf", format="pdf")
 
     plt.close()
-
-    print(
-        f"\033[F\033[K{config_data['data'].format(**element)}",
-        flush=True,
-    )
