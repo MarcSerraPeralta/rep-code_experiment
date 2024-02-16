e1a6fa61e1ba936931253345f290d00e55b2f806
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-02-16 23:39:17 +0100

    Update classifiers and add 3-state one

--- dataset/run_three-state_classifier.py
+++ dataset/run_three-state_classifier.py
@@ -7,7 +7,7 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
-from iq_readout.two_state_classifiers import *
+from iq_readout.three_state_classifiers import *
 from rep_code.dataset import sequence_generator
 
 DATA_DIR = pathlib.Path(
@@ -16,7 +16,7 @@ DATA_DIR = pathlib.Path(
 
 EXP_NAME = "20230119_initial_data_d5"
 
-CLASSIFIER = DecayLinearClassifier
+CLASSIFIER = GaussMixClassifier
 
 P0 = 0.5  # probability of the qubit being in state 0
 
@@ -46,17 +46,17 @@ for element in sequence_generator(STRING_DATA):
     for qubit in iq_readout_data.qubit:
         # run first calibration
         qubit_name = qubit.values.item()
-        shots_0, shots_1 = (
+        shots_0, shots_1, shots_2 = (
             iq_readout_data.calibration.sel(state=i, qubit=qubit)
             .transpose("shot", "iq")
             .values
-            for i in range(2)
+            for i in range(3)
         )
-        classifier = CLASSIFIER().fit(shots_0, shots_1)
+        classifier = CLASSIFIER().fit(shots_0, shots_1, shots_2)
         calibration_params_no_ps[qubit_name] = classifier.params()
 
         # plot readout calibration without PS
-        fig = summary(classifier, shots_0, shots_1)
+        fig = summary(classifier, shots_0, shots_1, shots_2)
         for format_ in ["pdf"]:
             fig.savefig(
                 cal_dir / f"{cla_name}_{qubit_name}_no-ps.{format_}",
@@ -65,27 +65,32 @@ for element in sequence_generator(STRING_DATA):
         plt.close()
 
         # run PS based on heralded init
-        her_init_0, her_init_1 = (
+        her_init_0, her_init_1, her_init_2 = (
             iq_readout_data.heralded_init.sel(state=i, qubit=qubit)
             .transpose("shot", "heralded_rep", "iq")
             .values
-            for i in range(2)
+            for i in range(3)
         )
         her_init_0 = classifier.predict(her_init_0, p0=P0)
         her_init_1 = classifier.predict(her_init_1, p0=P0)
+        her_init_2 = classifier.predict(her_init_2, p0=P0)
         mask_ps_0 = ~her_init_0.sum(axis=1).astype(bool)
         mask_ps_1 = ~her_init_1.sum(axis=1).astype(bool)
-        shots_0_ps, shots_1_ps = shots_0[mask_ps_0], shots_1[mask_ps_1]
+        mask_ps_2 = ~her_init_2.sum(axis=1).astype(bool)
+        shots_0_ps = shots_0[mask_ps_0]
+        shots_1_ps = shots_1[mask_ps_1]
+        shots_2_ps = shots_2[mask_ps_2]
         ps_fraction.append(len(shots_0_ps) / len(shots_0))
         ps_fraction.append(len(shots_1_ps) / len(shots_1))
+        ps_fraction.append(len(shots_2_ps) / len(shots_2))
 
         # run second calibration
-        classifier = CLASSIFIER().fit(shots_0_ps, shots_1_ps)
+        classifier = CLASSIFIER().fit(shots_0_ps, shots_1_ps, shots_2_ps)
         calibration_params_ps[qubit_name] = classifier.params()
 
         # plot readout calibration without PS
         fig, ax = plt.subplots()
-        fig = summary(classifier, shots_0_ps, shots_1_ps)
+        fig = summary(classifier, shots_0_ps, shots_1_ps, shots_2_ps)
         for format_ in ["pdf", "png"]:
             fig.savefig(
                 cal_dir / f"{cla_name}_{qubit_name}_ps.{format_}",
@@ -93,8 +98,8 @@ for element in sequence_generator(STRING_DATA):
             )
         plt.close()
 
-    np.save(cal_dir / f"2state_{cla_name}_params_no-ps.npy", calibration_params_no_ps)
-    np.save(cal_dir / f"2state_{cla_name}_params_ps.npy", calibration_params_ps)
+    np.save(cal_dir / f"3state_{cla_name}_params_no-ps.npy", calibration_params_no_ps)
+    np.save(cal_dir / f"3state_{cla_name}_params_ps.npy", calibration_params_ps)
 
     print(
         f"\033[F\033[K{cal_dir} ps_fraction={np.average(ps_fraction):0.5f}",
